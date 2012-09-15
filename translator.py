f9536b1f93ab88b536c52732524e177d56eb357a
KennyTM~ <kennytm@gmail.com>
2012-09-16 02:28:19 +0800

    Just add some comments.

--- translator.py
+++ translator.py
@@ -27,22 +27,43 @@ INTEGER_KINDS = {
 
 FLOAT_KINDS = {TypeKind.FLOAT, TypeKind.DOUBLE, TypeKind.LONGDOUBLE}
 
+#-------------------------------------------------------------------------------
+
 _unique_str_counter = 0
 def unique_str():
+    """
+    Obtain a globally-unique identifier.
+    """
     global _unique_str_counter
     _unique_str_counter += 1
-    return 'xx_temp_' + str(_unique_str_counter)
+    return '_x_' + str(_unique_str_counter)
+
 
 def capitalize(s):
+    """
+    Captialize a string without causing the other characters to become
+    lowercase.
+    """
     return s[0].upper() + s[1:]
 
 def strip_prefix_and_camelize(s):
+    """
+    Remove the prefix of a string, and change it to CamelCase, e.g.::
+
+        >>> strip_prefix_and_camelize("cairo_push_group_with_content")
+        pushGroupWithContent
+    """
     arr = s.split('_')
     if not arr[0]:
         arr = arr[1:]
     return arr[1] + ''.join(p.capitalize() for p in arr[2:])
 
+#-------------------------------------------------------------------------------
+
 def is_c_string(typ):
+    """
+    Check whether a clang Type is a C string, i.e. ``const char*``.
+    """
     if typ.kind != TypeKind.POINTER:
         return False
     pointee = typ.get_pointee()
@@ -50,12 +71,21 @@ def is_c_string(typ):
            pointee.is_const_qualified()
 
 def is_primitive_type(typ):
+    """
+    Check whether a clang Type is a primitive type directly representable in Oz,
+    i.e. boolean, integers or floats.
+    """
     kind = typ.kind
     return kind == TypeKind.BOOL or kind in INTEGER_KINDS or kind in FLOAT_KINDS
 
-def create_encode_statements(typ, source_name, target_name):
-    "Create the statements encoding a C value into an Oz UnstableNode."
+#-------------------------------------------------------------------------------
 
+def create_encode_statements(typ, source_name, target_name):
+    """
+    Create a list of C++ declarations and statements encoding a C value into an
+    Oz UnstableNode. The target names must not already exist, since they will be
+    declared in the statements returns from this function.
+    """
     if is_primitive_type(typ):
         return ["auto %s = build(vm, %s);" % (target_name, source_name)]
 
@@ -98,8 +128,10 @@ def create_encode_statements(typ, source_name, target_name):
                                   (source_name, target_name, typ.kind.spelling.decode('utf-8')))
 
 def create_decode_statements(typ, source_name, target_name):
-    "Create the statements decoding an Oz RichNode into a pre-declared C value."
-
+    """
+    Create a list of C++ statements decoding an Oz RichNode into a C value. The
+    target variable must already been declared.
+    """
     if is_c_string(typ):
         return ["""
             auto %(uniq)s = vsToString<char>(vm, %(src)s);
@@ -141,6 +173,12 @@ def create_decode_statements(typ, source_name, target_name):
                                   (source_name, target_name, typ.kind.spelling.decode('utf-8')))
 
 def get_arg_spec(function, func_name):
+    """
+    Get the C++ function definition from a clang function Cursor. The result is
+    a 2-tuple, with the first being a C++ code of the signature of the Oz
+    built-in procedure (e.g. ``, In arg1, Out arg2``), and the second being a
+    list of C++ statements of the built-in procedure.
+    """
     cc_statements = []
     arg_names = []
     arg_inouts = []
@@ -170,6 +208,21 @@ def get_arg_spec(function, func_name):
     return (arg_proto, cc_statements)
 
 def create_datatype(struct_name, is_pointer):
+    """
+    Create a Mozart/Oz DataType, given a C structure name. The DataType will be
+    created like this:
+
+    1. If the structure is incomplete, the parameter ``is_pointer`` should be
+       set to True. The generated DataType will then be based on a C pointer of
+       that structure.
+    2. Otherwise, ``is_pointer`` should be False, and the DataType will contain
+       the whole structure by value.
+
+    In both cases, the data type will contain a ``value()`` method to return the
+    pointer to the structure. The name of the DataType is
+    ``D_(name of C structure)``.
+    """
+
     params = {'s': struct_name}
 
     statements = ["""
@@ -213,7 +266,7 @@ def create_datatype(struct_name, is_pointer):
                 D_%(s)s(VM vm, const %(s)s& value) : _value(value) {}
                 D_%(s)s(VM vm, GR gr, Self from) : _value(from->_value) {}
 
-                %(s)s* value() { return &_value; }
+                const %(s)s* value() const { return &_value; }
 
             private:
                 %(s)s _value;
@@ -222,9 +275,13 @@ def create_datatype(struct_name, is_pointer):
 
     return statements
 
-
+#-------------------------------------------------------------------------------
 
 class Translator:
+    """
+    This class exists mainly just to hold the states.
+    """
+
     def __init__(self, module):
         self._module = module
         self._cc_file = open(join(SRC, module + CC_EXT), 'w')
@@ -251,6 +308,9 @@ class Translator:
         return handled
 
     def _print_header(self):
+        """
+        Print the C++ headers to all files.
+        """
         format_params = {
             'guard': 'M2G3_MODULE_' + self._module.upper() + "_" + unique_str(),
             'mod': self._module,
@@ -299,6 +359,9 @@ class Translator:
 
 
     def _print_footer(self):
+        """
+        Print the C++ footers to all files.
+        """
         self._hh_file.write("""
             };
 
@@ -324,6 +387,9 @@ class Translator:
         """)
 
     def _print_function(self, function):
+        """
+        Print a single function.
+        """
         source_func_name = function.spelling.decode('utf-8')
         (arg_proto, cc_statements) = get_arg_spec(function, source_func_name)
 
@@ -351,6 +417,9 @@ class Translator:
         self._cc_file.write("}\n")
 
     def _collect_structure(self, typedef):
+        """
+        Collection the structures cursors.
+        """
         type_name = typedef.spelling.decode('utf-8')
         underlying_type = typedef.underlying_typedef_type
         if underlying_type.kind != TypeKind.UNEXPOSED:
@@ -364,12 +433,18 @@ class Translator:
             self._structs[type_name] = is_complete
 
     def _print_structures(self):
+        """
+        Print all structures as DataTypes.
+        """
         for struct_name, is_complete in self._structs.items():
             data_type = create_datatype(struct_name, not is_complete)
             self._types_decl_hh_file.writelines(data_type)
             self._types_hh_file.write('#include "D_%s-implem.hh"\n' % struct_name)
 
     def print(self):
+        """
+        Print everything into the files.
+        """
         self._print_header()
 
         tu = TranslationUnit.from_source(join(C_FILES, self._module + C_EXT))
